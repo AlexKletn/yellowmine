bb58c24abc879945b8848e38eaaa8ef458823d01
Alex Kletnyy <q@obscur.space>
2024-03-22 16:46:02 +0300

    chore: init

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import globals from 'globals';
+import stylistic from '@stylistic/eslint-plugin';
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+import rules from './eslint-config/rules.js';
+
+export default [
+  ...tseslint.config(eslint.configs.recommended,
+    ...tseslint.configs.recommended),
+  stylistic.configs.customize({
+    semi: true,
+    jsx: true,
+    indent: 2,
+  }),
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  {
+    rules: {
+      'prefer-const': 'error',
+      ...rules,
+    },
+    files: ['**/*.{ts}'],
+  },
+  {
+    ignores: [
+      'dist',
+    ],
+  },
+];
